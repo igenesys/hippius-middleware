5fb8702c916cd52de5c54f99cdca4840c3715f04
Jenish Mandalia <jenish@i-genesys.com>
2025-04-25 17:57:52 +0530

    Update installation

---
+++ hippius/hippius/hippius.py
@@ -0,0 +1,7 @@
+class Hippius:
+    def __init__(self, mnemonic):
+        self.mnemonic = mnemonic
+
+    def upload(self, file_path):
+        # Dummy simulation: Replace with real upload logic later
+        return "QmDummyCID1234567890Example"
